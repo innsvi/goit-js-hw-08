51826111b0141d4757d5bfaa2cb39d01fc86e0ae
Inna Svichkar <112700457+innsvi@users.noreply.github.com>
2023-02-25 15:41:46 +0100

    Final updates

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.insertAdjacentHTML('afterbegin', cardGallery(galleryItems))
+
+function cardGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class = "gallery__item" href = "${original}"/>
+             <img class = "gallery__image"
+            src = "${preview}"           
+            alt = "${description}" />
+        </a>
+      </div>`;
+    }).join('');  
+}
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 300,
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
+
+const formEl = document.querySelector('.feedback-form');
+const refs = {
+  email: document.querySelector('input'),
+  message: document.querySelector('textarea'),
+};
+
+const FORM_STATE = 'feedback-form-state';
+
+if (localStorage.getItem(FORM_STATE)) {
+  refs.email.value = JSON.parse(localStorage.getItem(FORM_STATE)).email || '';
+  refs.message.value =
+    JSON.parse(localStorage.getItem(FORM_STATE)).message || '';
+}
+
+let result = {};
+
+formEl.addEventListener('input', throttle(handleFormInput, 500));
+
+formEl.addEventListener('submit', handlerFormSubmit);
+
+function handleFormInput(e) {
+  result[e.target.name] = e.target.value;
+  localStorage.setItem(FORM_STATE, JSON.stringify(result));
+}
+
+function handlerFormSubmit(e) {
+  e.preventDefault();
+  formEl.reset();
+  localStorage.removeItem(FORM_STATE);
+};
 
