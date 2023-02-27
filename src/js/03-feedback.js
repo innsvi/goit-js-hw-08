893b87cd723e26752b661ab1ff48f3d589954e34
Inna Svichkar <112700457+innsvi@users.noreply.github.com>
2023-02-27 23:15:58 +0100

    Added saved data function

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,33 +1,50 @@
 import throttle from 'lodash.throttle';
 
-const formEl = document.querySelector('.feedback-form');
+// const formEl = document.querySelector('.feedback-form');
+
 const refs = {
-  email: document.querySelector('input'),
-  message: document.querySelector('textarea'),
+form: document.querySelector('.feedback-form'),
+mail: document.querySelector('input'),
+message: document.querySelector('textarea'),
 };
 
 const FORM_STATE = 'feedback-form-state';
 
-if (localStorage.getItem(FORM_STATE)) {
-  refs.email.value = JSON.parse(localStorage.getItem(FORM_STATE)).email || '';
-  refs.message.value =
-    JSON.parse(localStorage.getItem(FORM_STATE)).message || '';
-}
+// if (localStorage.getItem(FORM_STATE)) {
+//   refs.email.value = JSON.parse(localStorage.getItem(FORM_STATE)).email || '';
+//   refs.message.value =
+//     JSON.parse(localStorage.getItem(FORM_STATE)).message || '';
+// }
+
 
 let result = {};
 
-formEl.addEventListener('input', throttle(handleFormInput, 500));
+refs.form.addEventListener('input', throttle(handleFormInput, 500));
+refs.form.addEventListener('submit', handlerFormSubmit);
 
-formEl.addEventListener('submit', handlerFormSubmit);
+populateInputData();
 
-function handleFormInput(e) {
-  result[e.target.name] = e.target.value;
+function handleFormInput() {
+  result.email = refs.mail.value;
+  result.message = refs.message.value;
   localStorage.setItem(FORM_STATE, JSON.stringify(result));
 }
 
 function handlerFormSubmit(e) {
   e.preventDefault();
-  formEl.reset();
+  
+  console.log(populateInputData())
+
   localStorage.removeItem(FORM_STATE);
+  e.target.reset();
 };
+
+function populateInputData(){
+    const savedData = JSON.parse(localStorage.getItem(FORM_STATE));
+    if(savedData){
+        refs.mail.value = savedData.email;
+        refs.message.value = savedData.message;
+        return savedData
+    }
+}
 
